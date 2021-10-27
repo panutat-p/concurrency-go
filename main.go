be591a471d62d949a2d46e6413062d04c6f4badb
panutat-p <pp.panutat@gmail.com>
2021-10-27 14:54:26 +0700

    single thread, request to multiple URLs

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+	fmt.Println(link, "is up!")
+}
+
+func main() {
+	links := []string{
+		"https://amazon.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://example.com",
+		"https://golang.org",
+		"https://cryptobubbles.net",
+		"https://youtube.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
