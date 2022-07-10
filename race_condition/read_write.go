d067f4cc9e84bf762bea4591fd97ae2ca84349d6
panutat-p <pp.panutat@gmail.com>
2022-07-10 23:29:20 +0700

    add note about race condition

--- race_condition/read_write.go
+++ race_condition/read_write.go
@@ -3,6 +3,9 @@ package race_condition
 // ReadAndWrite
 // cd race_condition
 // go test -race
+// build with the so-called "race detector" enabled.
+// Data races are a programming error and any program with a data-race is invalid and its behaviour is undefined.
+
 func ReadAndWrite() {
 	var value int
 	var count int
