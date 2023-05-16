2ef4b25980909a442fe2e82c301a575c18ce341f
panutat-p <pp.panutat@gmail.com>
2023-05-16 21:35:02 +0700

    refactor: use race detector

--- race_condition/read_write_test.go
+++ race_condition/read_write_test.go
@@ -2,9 +2,8 @@ package race_condition
 
 import "testing"
 
-// run with "race detector"
-// cd race_condition
-// go test -race
+// run with 'race detector'
+// go test ./race_condition -race
 func TestReadAndWrite(t *testing.T) {
 	ReadAndWrite()
 }
